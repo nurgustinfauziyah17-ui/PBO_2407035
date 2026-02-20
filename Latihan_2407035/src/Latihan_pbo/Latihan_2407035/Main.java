051f0ffb5cde4cf4e3f6b4b6b95ac2bf1d220dc2
zia <nurgustinfauziyah17@gmail.com>
2026-02-20 14:46:29 +0700

    menambahkan Main java

--- Latihan_2407035/src/Latihan_pbo/Latihan_2407035/Main.java
+++ Latihan_2407035/src/Latihan_pbo/Latihan_2407035/Main.java
@@ -1,7 +1,34 @@
 package Latihan_pbo.Latihan_2407035;
 
+class Cafe{
+    String nama;
+    String alamat;
+    String menu;
+
+    //contrukstor
+    Cafe(String nama, String alamat, String menu) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.menu = menu;
+        System.out.println("Nama Cafe  : " + nama);
+        System.out.println("Alamat     : " + alamat);
+        System.out.println("Menu       : " + menu);
+        System.out.println();
+    }
+
+    //method
+    
+    void buka(){
+        System.out.println("Cafe sudah buka");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("praktikum 1");
+
+        Cafe cafe1 = new Cafe("jerami", "Karangampel", "Mie Goreng");
+        cafe1.buka();
+        Cafe cafe2 = new Cafe("kozi", "Indramayu Jabar", "Roti Bakar");
+        cafe2.buka();
     }
 }
